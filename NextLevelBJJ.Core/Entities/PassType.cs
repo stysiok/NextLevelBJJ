876cb52c3c9d783a9755941ad57f45872ea569c5
Grzegorz <stysiok@o2.pl>
2019-07-10 20:33:13 +0200

    Repository and PassType model enhancement

--- NextLevelBJJ.Core/Entities/PassType.cs
+++ NextLevelBJJ.Core/Entities/PassType.cs
@@ -8,8 +8,55 @@ namespace NextLevelBJJ.Core.Entities
     {
         public Guid Id { get; private set; }
         public string Name { get; private set; }
-        public int Price { get; private set; }
+        public decimal Price { get; private set; }
         public int Entries { get; private set; }
         public bool IsOpen { get; private set; }
+
+        private PassType()
+        {
+        }
+
+        public PassType(Guid id, string name, decimal price, int entries, bool isOpen)
+        {
+            Id = Guid.Empty == id ? Guid.NewGuid() : id;
+            SetName(name);
+            SetPrice(price);
+            SetEntries(entries);
+            SetIsOpen(isOpen);
+        }
+        private void SetName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name cannot be empty", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        private void SetPrice(decimal price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentException("Price cannot be lower than 0", nameof(price));
+            }
+
+            Price = price;
+        }
+
+        private void SetEntries(int entries)
+        {
+            if (entries < 0)
+            {
+                throw new ArgumentException("Entries number cannot be lower than 0", nameof(entries));
+            }
+
+            Entries = entries;
+        }
+
+        private void SetIsOpen(bool isOpen)
+        {
+            IsOpen = isOpen;
+        }
     }
 }
---
+++ NextLevelBJJ.Core/Repositories/IPassTypesRepository.cs
@@ -0,0 +1,15 @@
+﻿using NextLevelBJJ.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextLevelBJJ.Core.Repositories
+{
+    public interface IPassTypesRepository
+    {
+        Task<PassType> GetPassTypeAsync(Guid id);
+        Task AddPassTypeAsync(PassType passType);
+        Task DeletePassTypeAsync(Guid id);
+    }
+}
