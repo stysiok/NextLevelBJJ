b73d36f9a624e8cc48456380410d5a88d5910959
Grzegorz <stysiok@o2.pl>
2019-07-26 18:07:03 +0200

    Added validators

--- NextLevelBJJ.Api/Startup.cs
+++ NextLevelBJJ.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,9 @@ namespace NextLevelBJJ.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Application.IValidator>());
 
             services.AddInfrastructure();
         }
---
+++ NextLevelBJJ.Application/PassTypes/Commands/Validators/CreatePassTypeValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NextLevelBJJ.Application.PassTypes.Commands.Validators
+{
+    public class CreatePassTypeValidator : AbstractValidator<CreatePassType>
+    {
+        public CreatePassTypeValidator()
+        {
+            RuleFor(pt => pt.Name).NotEmpty();
+            RuleFor(pt => pt.Entries).GreaterThan(0);
+            RuleFor(pt => pt.Price).GreaterThan(0);
+        }
+    }
+}
