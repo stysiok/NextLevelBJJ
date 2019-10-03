504d43016b5f56aaaf42b7d446150fae2ef849ee
Grzegorz Stysiak <stysiok@o2.pl>
2019-10-03 21:49:42 +0200

    pass & passtypes configuration files

---
+++ src/NextLevelBJJ.Infrastructure/EF/Configurations/PassTypeConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NextLevelBJJ.Infrastructure.EF.Configurations
+{
+    public class PassTypeConfiguration
+    {
+        public PassTypeConfiguration()
+        {
+        }
+    }
+}
